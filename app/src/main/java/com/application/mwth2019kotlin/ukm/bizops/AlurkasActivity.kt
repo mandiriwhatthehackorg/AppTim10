98e1b368f1f3bfb6cb2a6ddb05f85621d5e6484b
Umam Fajri <umam.505@gmail.com>
2019-07-20 20:43:52 +0700

    alur kas

--- app/src/main/java/com/application/mwth2019kotlin/ukm/bizops/AlurkasActivity.kt
+++ app/src/main/java/com/application/mwth2019kotlin/ukm/bizops/AlurkasActivity.kt
@@ -1,13 +1,21 @@
 package com.application.mwth2019kotlin.ukm.bizops
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.application.mwth2019kotlin.R
 import kotlinx.android.synthetic.main.activity_alurkas.*
+import kotlinx.android.synthetic.main.layout_formtambahkas.view.*
+import kotlinx.android.synthetic.main.layout_formtambahkas_selasai.view.*
+
 
 class AlurkasActivity : AppCompatActivity() {
+    private lateinit var alertDialog: AlertDialog
+    private lateinit var alertDialog1: AlertDialog
 
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alurkas)
@@ -25,6 +33,33 @@ class AlurkasActivity : AppCompatActivity() {
         listModelAlurKas.add(ModelAlurKas("Judul Kas","Rp. 10.000","115001230131","22 - April - 2019","Pembelanjaan Buah Melon 5 Kg"))
         listModelAlurKas.add(ModelAlurKas("Judul Kas","Rp. 10.000","115001230131","22 - April - 2019","Pembelanjaan Buah Melon 5 Kg"))
         adapterAlurKas.setDatas(listModelAlurKas)
+
+        fab_addalurkas.setOnClickListener {
+            val dialogView = layoutInflater.inflate(R.layout.layout_formtambahkas, null);
+            dialogView.text_tambah.setOnClickListener {
+                alertDialog.dismiss()
+                val dialogView1 = layoutInflater.inflate(R.layout.layout_formtambahkas_selasai, null);
+                dialogView1.text_oke.setOnClickListener {
+                    alertDialog1.dismiss()
+                }
+                val alertDialogBuilder = AlertDialog.Builder(
+                    this
+                )
+                alertDialogBuilder.setView(dialogView1)
+                alertDialog1 = alertDialogBuilder.create()
+
+                // menampilkan alert dialog
+                alertDialog1.show()
+            }
+            val alertDialogBuilder = AlertDialog.Builder(
+                this
+            )
+            alertDialogBuilder.setView(dialogView)
+            alertDialog = alertDialogBuilder.create()
+
+            // menampilkan alert dialog
+            alertDialog.show()
+        }
 
     }
 }
